f879d883620812087511119bd7a5c3bed05f136b
Aditya Gupta <guptaditya81@gmail.com>
2025-07-22 14:01:08 +0530

    Fix: resolved import conflicts and routing issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({babel: {
+  plugins: [
+    react({
+      babel: {
         plugins: ['styled-jsx/babel'],
-      },}), tailwindcss()  ],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
